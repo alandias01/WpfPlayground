a43fdf3cc6db72c1ff231eba92167d4e64fb8c1e
alan <alandias01@yahoo.com>
2020-03-15 14:37:58 -0400

    ControlTemplate01.xaml

---
+++ WpfPlayground/Templates/ControlTemplates/ControlTemplate01.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfPlayground.Templates.ControlTemplates
+{
+    /// <summary>
+    /// Interaction logic for ControlTemplate01.xaml
+    /// </summary>
+    public partial class ControlTemplate01 : Window
+    {
+        public ControlTemplate01()
+        {
+            InitializeComponent();
+        }
+    }
+}
