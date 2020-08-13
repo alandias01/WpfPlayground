c53e4cf01b7d9dd3915390dc78dfd97fdf399b2f
alan <alandias01@yahoo.com>
2020-08-13 13:43:09 -0400

    TPL Dataflow

--- WpfPlayground/InterviewPractice/TPLDataflowView.xaml.cs
+++ WpfPlayground/InterviewPractice/TPLDataflowView.xaml.cs
@@ -2,12 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks.Dataflow;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -27,6 +29,13 @@ namespace WpfPlayground.InterviewPractice
 
     public class TPLDataflowViewModel : BindableBase
     {
+        private string output;
 
+        public string Output { get => output; set => this.SetProperty(ref output, value); }
+        public TPLDataflowViewModel()
+        {
+            this.Output = "Hello";
+            //var ablock = new ActionBlock<int>(input => Console.WriteLine(input));
+        }
     }
 }
