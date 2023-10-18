f3126e961ace48093dfc132ce9b000b20b2a2827
alan <alandias01@yahoo.com>
2023-10-18 19:12:33 -0400

    FilterListByMultipleItems

--- WpfPlayground/Playground.cs
+++ WpfPlayground/Playground.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace WpfPlayground
@@ -8,7 +9,33 @@ namespace WpfPlayground
     {
         public Playground()
         {
-
+            filter1();
         }
+
+        private void FilterListByMultipleItems()
+        {
+            /* Result has full list
+             * User enters multiple items in arr
+             * Create new List<CtpyAcct> thats filtered by items in arr
+             */
+
+            List<CtpyAcc> result = new List<CtpyAcc> {
+                new CtpyAcc { ctpy = "aa", acct = 1 }, 
+                new CtpyAcc { ctpy = "bb", acct = 2 }, 
+                new CtpyAcc { ctpy = "cc", acct = 3 },
+                new CtpyAcc { ctpy = "dd", acct = 4 },
+                new CtpyAcc { ctpy = "ee", acct = 5 }
+            };
+
+            string[] arr = { "bb", "dd" };
+
+            var filteredList = result.Where(x => arr.Any(y => y == x.ctpy));
+        }
+    }
+
+    public class CtpyAcc
+    {
+        public string ctpy;
+        public int acct;
     }
 }
