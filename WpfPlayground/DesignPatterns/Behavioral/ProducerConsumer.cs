7f79d55b61bc0332c90264fc1fa2b04c0d20dd3f
alan <alandias01@yahoo.com>
2025-05-13 22:38:22 -0400

    update

--- WpfPlayground/DesignPatterns/Behavioral/ProducerConsumer.cs
+++ WpfPlayground/DesignPatterns/Behavioral/ProducerConsumer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace WpfPlayground.DesignPatterns.Behavioral
@@ -15,9 +16,39 @@ namespace WpfPlayground.DesignPatterns.Behavioral
      */
     public class ProducerConsumer
     {
-        public ProducerConsumer()
+        public ProducerConsumer() => BasicProducerConsumer();
+
+        private void BasicProducerConsumer()
         {
+            var channel = Channel.CreateUnbounded<int>();
+            Task p = Task.Run(() => Enumerable.Range(0, 10).ToList().ForEach(x => channel.Writer.TryWrite(x)));
             
+        }
+
+        private void ProducerConsumer01()
+        {
+            var channel = Channel.CreateBounded<int>(new BoundedChannelOptions(5) { FullMode = BoundedChannelFullMode.Wait });
+
+            Task p = Task.Run(async () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    await channel.Writer.WriteAsync(i);
+                    await Task.Delay(1000);
+                }
+                channel.Writer.Complete();
+            });
+
+            Task c = Task.Run(async () =>
+            {
+                await foreach (var item in channel.Reader.ReadAllAsync())
+                {
+                    Console.WriteLine($"Consuming {item}");
+                    await Task.Delay(1000);
+                }
+            });
+
+            Task.WhenAll(p, c);            
         }
     }
 }
